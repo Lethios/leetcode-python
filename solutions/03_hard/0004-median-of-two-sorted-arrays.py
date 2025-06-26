23828edc68b3c3e2300878ed59da90bd914acbb2
Lethios <204111230+Lethios@users.noreply.github.com>
2025-06-26 12:14:37 +0530

    Update 0004-median-of-two-sorted-arrays.py

--- solutions/03_hard/0004-median-of-two-sorted-arrays.py
+++ solutions/03_hard/0004-median-of-two-sorted-arrays.py
@@ -2,10 +2,29 @@
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        merged_arr = [*nums1, *nums2]
-        merged_arr.sort()
+        arr_len = len(nums1) + len(nums2)
+        middle_index = arr_len // 2
 
-        if len(merged_arr) % 2 != 0:
-            return merged_arr[(len(merged_arr) + 1) // 2 - 1]
+        merged_arr = []
+        i, j = 0, 0
+        count = 0
+
+        while count <= middle_index:
+            if i >= len(nums1):
+                merged_arr.append(nums2[j])
+                j += 1
+            elif j >= len(nums2):
+                merged_arr.append(nums1[i])
+                i += 1
+            elif nums1[i] < nums2[j]:
+                merged_arr.append(nums1[i])
+                i += 1
+            else:
+                merged_arr.append(nums2[j])
+                j += 1
+            count += 1
+
+        if arr_len % 2 == 0:
+            return (merged_arr[middle_index - 1] + merged_arr[middle_index]) / 2
         else:
-            return (merged_arr[(len(merged_arr) // 2) - 1] + merged_arr[(len(merged_arr) // 2)]) / 2
+            return (merged_arr[middle_index])
