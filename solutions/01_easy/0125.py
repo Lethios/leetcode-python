7127041c55634891f3f8096320f603130de355eb
Lethios <204111230+Lethios@users.noreply.github.com>
2025-09-08 10:41:13 +0530

    Update 0125.py

--- solutions/01_easy/0125.py
+++ solutions/01_easy/0125.py
@@ -1,1 +1,18 @@
+# https://leetcode.com/problems/valid-palindrome
 
+from string import ascii_letters, digits
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        letter_set = set(ascii_letters + digits)
+        clean = ''.join(char.lower() for char in s if char in letter_set)
+
+        l, r = 0, len(clean) - 1
+        while l < r:
+            if clean[l] != clean[r]:
+                return False
+
+            l += 1
+            r -= 1
+       
+        return True
