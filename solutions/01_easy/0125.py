0b12f063625c1316bc8ca00c083e3b1d6c760fe5
Lethios <204111230+Lethios@users.noreply.github.com>
2025-09-08 10:49:54 +0530

    Update 0125.py
    Shortened code to use string slicing

--- solutions/01_easy/0125.py
+++ solutions/01_easy/0125.py
@@ -4,12 +4,4 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         clean = ''.join(char.lower() for char in s if char.isalnum())
 
-        l, r = 0, len(clean) - 1
-        while l < r:
-            if clean[l] != clean[r]:
-                return False
-
-            l += 1
-            r -= 1
-       
-        return True
+        return clean == clean[::-1]
