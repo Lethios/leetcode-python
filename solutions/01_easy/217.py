3405405bef83a03553c59bdad2bfb8ce31bd7936
Lethios <204111230+Lethios@users.noreply.github.com>
2025-09-08 08:18:59 +0530

    Update 217.py

--- solutions/01_easy/217.py
+++ solutions/01_easy/217.py
@@ -1,1 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        unique_set = set()
 
+        for digit in nums:
+            if digit in unique_set:
+                return True
+        
+            unique_set.add(digit)
+        
+        return False
