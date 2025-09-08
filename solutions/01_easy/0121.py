f013bc8603eac1ba3784830c863d0b915440ffa7
Lethios <204111230+Lethios@users.noreply.github.com>
2025-09-08 08:07:21 +0530

    Update 0121.py

--- solutions/01_easy/0121.py
+++ solutions/01_easy/0121.py
@@ -1,1 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        min_price = math.inf
 
+        for price in prices:
+            min_price = min(price, min_price)
+            max_profit = max(price - min_price, max_profit)
+
+        return max_profit
