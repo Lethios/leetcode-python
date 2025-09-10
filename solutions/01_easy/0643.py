62d79e13c45360bff713140cb77045120a5230e4
Lethios <204111230+Lethios@users.noreply.github.com>
2025-09-10 11:43:25 +0530

    Update 0643.py

--- solutions/01_easy/0643.py
+++ solutions/01_easy/0643.py
@@ -1,1 +1,21 @@
+# https://leetcode.com/problems/maximum-average-subarray-i
 
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        if len(nums) == 1:
+            return nums[0]
+
+        l = sum = max_sum = 0
+        
+        for r, num in enumerate(nums):
+            sum = sum + num
+
+            if r == k - 1:
+                max_sum = sum
+            elif r > k - 1:
+                sum = sum - nums[l]
+                l += 1
+            
+            max_sum = sum if sum > max_sum else max_sum
+
+        return round(max_sum / k, 5)
