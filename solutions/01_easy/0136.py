c46ae57c5d811a31676d78745abd5d6982c7304c
Lethios <204111230+Lethios@users.noreply.github.com>
2025-09-10 14:18:23 +0530

    Update 0136.py

--- solutions/01_easy/0136.py
+++ solutions/01_easy/0136.py
@@ -1,1 +1,11 @@
+# https://leetcode.com/problems/single-number/
 
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        valid_set = set()
+
+        for num in nums:
+            valid_set.add(num) if num not in valid_set else valid_set.remove(num)
+
+        for char in valid_set:
+            return char
