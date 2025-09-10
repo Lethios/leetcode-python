b8217e0bf2acdb6254211cbb9760581cc774ac42
Lethios <204111230+Lethios@users.noreply.github.com>
2025-09-10 14:26:12 +0530

    Update 0136.py
    Better solution using XOR reduces runtime to 0ms

--- solutions/01_easy/0136.py
+++ solutions/01_easy/0136.py
@@ -2,10 +2,9 @@
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        valid_set = set()
+        single = 0
 
         for num in nums:
-            valid_set.add(num) if num not in valid_set else valid_set.remove(num)
-
-        for char in valid_set:
-            return char
+            single ^= num
+        
+        return single
