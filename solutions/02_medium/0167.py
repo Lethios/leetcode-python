3499453fde12a0296765d5f87f9886af1d3726d8
Lethios <204111230+Lethios@users.noreply.github.com>
2025-09-10 12:10:41 +0530

    Update 0167.py

--- solutions/02_medium/0167.py
+++ solutions/02_medium/0167.py
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
