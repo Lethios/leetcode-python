a7aad9ca0b4df3506b3c7c227a0f463690be5a15
Lethios <204111230+Lethios@users.noreply.github.com>
2025-09-10 09:31:40 +0530

    Update 0485.py

--- solutions/01_easy/0485.py
+++ solutions/01_easy/0485.py
@@ -1,1 +1,8 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        count = longest = 0
+        for char in nums:
+            count = count + 1 if char == 1 else 0
+            longest = count if count > longest else longest
 
+        return longest
