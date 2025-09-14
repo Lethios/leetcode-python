743cdf504142ca1f830304fe9c59731ec43ff189
Lethios <204111230+Lethios@users.noreply.github.com>
2025-09-14 11:57:29 +0530

    Update 0338.py
    Improved runtime from 5ms to 3ms

--- solutions/01_easy/0338.py
+++ solutions/01_easy/0338.py
@@ -2,9 +2,9 @@
 
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans = [0] * (n + 1)
+        ans = [0]
 
         for i in range(1, n + 1):
-            ans[i] = ans[i & (i - 1)] + 1
+            ans.append(ans[i & (i - 1)] + 1)
         
         return ans
