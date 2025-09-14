572cabb694b8621a2e1957a8a90baea76aa06d77
Lethios <204111230+Lethios@users.noreply.github.com>
2025-09-14 10:56:44 +0530

    Update 0338.py

--- solutions/01_easy/0338.py
+++ solutions/01_easy/0338.py
@@ -1,1 +1,16 @@
+# https://leetcode.com/problems/counting-bits/
 
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = []
+
+        for i in range(n + 1):
+            counter = 0
+            j = i
+
+            while j > 0:
+                counter = counter + 1 if j & 1 == 1 else counter
+                j >>= 1
+            ans.append(counter)
+
+        return ans
