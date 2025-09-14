ddfb4b74e9ea065d241b22081f6a54b4ca85f0e6
Lethios <204111230+Lethios@users.noreply.github.com>
2025-09-14 11:52:08 +0530

    Update 0338.py
    Improved runtime to 7ms

--- solutions/01_easy/0338.py
+++ solutions/01_easy/0338.py
@@ -2,15 +2,9 @@
 
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans = []
+        ans = [0] * (n + 1)
 
-        for i in range(n + 1):
-            counter = 0
-            j = i
-
-            while j > 0:
-                counter = counter + 1 if j & 1 == 1 else counter
-                j >>= 1
-            ans.append(counter)
-
+        for i in range(1, n + 1):
+            ans[i] = ans[i & (i - 1)] + 1
+        
         return ans
