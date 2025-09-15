09f56ccc3f41d044c5fe24fe930868283ba3e444
Lethios <204111230+Lethios@users.noreply.github.com>
2025-09-15 10:49:01 +0530

    Create 01935.py

---
+++ solutions/01935.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        invalid = set(letter for letter in brokenLetters)
+        counter = 0
+
+        word_list = text.split(" ")
+        for word in word_list:
+            for letter in word:
+                if letter in invalid:
+                    counter += 1
+                    break
+
+        return len(word_list) - counter
