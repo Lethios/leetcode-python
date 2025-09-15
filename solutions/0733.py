56012711f8fd4fd6ee001a3d3ce62012fb5149b3
Lethios <204111230+Lethios@users.noreply.github.com>
2025-09-15 09:55:55 +0530

    Update 0733.py

--- solutions/0733.py
+++ solutions/0733.py
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/flood-fill
+# https://leetcode.com/problems/flood-fill/
 
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
