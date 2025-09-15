f5faee2ff692dee9d1217b85a9b71335392c8858
Lethios <204111230+Lethios@users.noreply.github.com>
2025-09-15 14:58:33 +0530

    Create 0169.py

---
+++ solutions/0169.py
@@ -0,0 +1,12 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate = None
+        counter = 0
+
+        for element in nums:
+            if counter == 0:
+                candidate = element
+            
+            counter = counter + 1 if element == candidate else counter - 1
+
+        return candidate
