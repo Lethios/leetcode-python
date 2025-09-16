82b1ebd56f3c2a44717c3d54b8faf4dc3e9f0124
Aryan Anand Kamath <204111230+Lethios@users.noreply.github.com>
2025-09-16 17:54:02 +0530

    Update 3684.py
    7ms to 0ms. I preferred my heap solution :/

--- solutions/3684.py
+++ solutions/3684.py
@@ -2,16 +2,4 @@
 
 class Solution:
     def maxKDistinct(self, nums: List[int], k: int) -> List[int]:
-        min_heap, unique = [], set()
-
-        for num in nums:
-            if num in unique:
-                continue
-            else:
-                unique.add(num)
-
-            heapq.heappush(min_heap, num)
-            if len(min_heap) > k:
-                heapq.heappop(min_heap)
-
-        return sorted(min_heap, reverse=True)
+        return sorted(list(set(nums)), reverse=True)[:k]
