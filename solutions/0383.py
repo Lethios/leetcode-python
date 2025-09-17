029f64b4b78002330a69df4c264d6b15459f5185
Aryan Anand Kamath <204111230+Lethios@users.noreply.github.com>
2025-09-17 11:32:10 +0530

    Create 0383.py

---
+++ solutions/0383.py
@@ -0,0 +1,5 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        note_count, magazine_count = Counter(ransomNote), Counter(magazine)
+
+        return note_count <= magazine_count
