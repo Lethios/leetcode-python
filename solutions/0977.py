b3c9a053d2887d400f2056c10028d53727297eaa
Lethios <204111230+Lethios@users.noreply.github.com>
2025-09-23 09:41:19 +0530

    Optimize sortedSquares to use two-pointer technique

--- solutions/0977.py
+++ solutions/0977.py
@@ -2,4 +2,15 @@
 
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        return sorted([num ** 2 for num in nums])
+        len_n, i_list = len(nums), [0] * len(nums)
+        l, r = 0, len_n - 1
+
+        for i in reversed(range(len_n)):
+            if abs(nums[l]) > abs(nums[r]):
+                i_list[i] = abs(nums[l]) ** 2
+                l += 1
+            else:
+                i_list[i] = abs(nums[r]) ** 2
+                r -= 1
+        
+        return i_list
