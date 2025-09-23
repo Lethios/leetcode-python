34b315e49847d96d86788dce944fd59a8e86bd51
Lethios <204111230+Lethios@users.noreply.github.com>
2025-09-23 09:41:09 +0530

    Implement sortedSquares function in Solution class
    Added a solution to compute squares of a sorted array.

--- solutions/0977.py
+++ solutions/0977.py
@@ -1,1 +1,5 @@
+# https://leetcode.com/problems/squares-of-a-sorted-array/
 
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        return sorted([num ** 2 for num in nums])
