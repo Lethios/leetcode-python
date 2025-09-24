011645aa92e62b7118738ca419ace8955a309c2d
Lethios <204111230+Lethios@users.noreply.github.com>
2025-09-24 22:13:20 +0530

    Update 0083.py

--- solutions/0083.py
+++ solutions/0083.py
@@ -1,1 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        current = scout = head
 
+        while scout and scout.next:
+            scout = scout.next
+            if scout.val != current.val:
+                current.next = scout
+                current = scout
+
+        if scout != current:
+            current.next = None
+
+        return head
