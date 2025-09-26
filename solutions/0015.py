8e8356bfbbc83c5d0320d86128e329c583849623
Lethios <204111230+Lethios@users.noreply.github.com>
2025-09-26 10:06:26 +0530

    Refactor threeSum method for better performance
    Refactor threeSum method to improve efficiency and remove duplicates handling.

--- solutions/0015.py
+++ solutions/0015.py
@@ -2,24 +2,28 @@
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        len_n, ans = len(nums), []
-        dup_set = set()
+        ans = []
         nums.sort()
 
-        for fix in range(0, len_n - 2):
-            i, j = fix + 1, len_n - 1
+        for fix in range(len(nums)):
+            if fix > 0 and nums[fix] == nums[fix - 1]:
+                continue
+
+            i = fix + 1
+            j = len(nums) - 1
 
             while i < j:
                 sum_n = nums[fix] + nums[i] + nums[j]
-                if sum_n < 0:
-                    i += 1
-                elif sum_n > 0:
-                    j -= 1
-                else:
-                    if (nums[fix], nums[i], nums[j]) not in dup_set:
-                        ans.append([nums[fix], nums[i], nums[j]])
-                        dup_set.add((nums[fix], nums[i], nums[j]))
 
+                if sum_n > 0:
+                    j -= 1
+                elif sum_n < 0:
                     i += 1
+                else:
+                    ans.append([nums[fix], nums[i], nums[j]])
+                    i += 1
+
+                    while nums[i] == nums[i - 1] and i < j:
+                        i += 1
 
         return ans
