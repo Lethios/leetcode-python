5fb72269949baadac42a871bf6aae91109189869
Lethios <204111230+Lethios@users.noreply.github.com>
2025-09-30 09:31:52 +0530

    Simplify triangular sum calculation

--- solutions/2221.py
+++ solutions/2221.py
@@ -5,7 +5,6 @@ class Solution:
         len_n = len(nums)
         for i in range(len_n - 1):
             for j in range(len_n - 1 - i):
-                sum_n = nums[j] + nums[j + 1]
-                nums[j] = sum_n if sum_n < 10 else sum_n % 10
+                nums[j] = (nums[j] + nums[j + 1]) % 10
 
         return nums[0]
