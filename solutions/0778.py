2ad4bcc0e01b70c3ff255d2ca90b8172021b2ea0
Lethios <204111230+Lethios@users.noreply.github.com>
2025-10-06 12:03:01 +0530

    corrected code

--- solutions/0778.py
+++ solutions/0778.py
@@ -7,23 +7,19 @@ class Solution:
         if n == 1:
             return 0
         else:
-            queue = deque([(0, 0)])
+            min_heap = []
 
-        visited, max_val = set(), 0
-        while queue:
-            i, j = queue.popleft()
-            visited.add((i, j))
+        heapq.heappush(min_heap, (grid[0][0], 0, 0))
+        visited = {(0, 0)}
 
-            lowest = None
+        while min_heap:
+            val, i, j = heapq.heappop(min_heap)
+            if i == j == n - 1:
+                return val
+
             for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
                 if 0 <= x < n and 0 <= y < n and (x, y) not in visited:
-                    val = grid[x][y]
-                    max_val = val if val > max_val else max_val
-                    if lowest is None:
-                        lowest = (x, y, val)
-                    else:
-                        lowest = (x, y, val) if val <= lowest[2] else lowest
-            if lowest is not None:
-                queue.append((lowest[0], lowest[1]))
+                    heapq.heappush(min_heap, (max(val, grid[x][y]), x, y))
+                    visited.add((x, y))
 
-        return max_val
+        return -1
