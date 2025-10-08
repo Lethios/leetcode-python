d6a8782213363deb2bafe214bc8fea7edb8d796f
Lethios <204111230+Lethios@users.noreply.github.com>
2025-10-08 09:44:32 +0530

    Update 2300.py

--- solutions/2300.py
+++ solutions/2300.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        potions.sort()
+        ans = []
+        for spell in spells:
+            left, right = 0, len(potions)
+            while left < right:
+                mid = (left + right) // 2
+                if spell * potions[mid] >= success:
+                    right = mid
+                else:
+                    left = mid + 1
+            ans.append(len(potions) - left)
+        
+        return ans
